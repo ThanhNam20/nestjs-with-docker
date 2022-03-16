2bc37c39caa3d5cac73ef657e147c0e50a125422
cao.sytran <cao.sytran@powergatesoftware.net>
2022-03-16 17:50:14 +0700

    Turn off authen

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,7 +7,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  @UseGuards(AuthGuard("jwt"))
+  // @UseGuards(AuthGuard("jwt"))
   getHello(): string {
     return this.appService.getHello();
   }
